57a7abde23963bd9586f14a07c9b4a13261d9edb
Guthax <jurrbuitenweg@gmail.com>
2025-02-25 14:10:06 +0100

    gradient viz

--- evaluator.py
+++ evaluator.py
@@ -2,6 +2,9 @@ import cv2
 import gym
 import numpy as np
 import torch
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget, RawScoresOutputTarget
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.preprocessing import preprocess_obs
 from stable_baselines3.common.utils import obs_as_tensor
@@ -10,13 +13,14 @@ from stable_baselines3.common.utils import obs_as_tensor
 class Evaluator:
     evaluation_environment: gym.Env
 
-    def __init__(self, eval_env: gym.Env, algorithm: BaseAlgorithm):
+    def __init__(self, eval_env: gym.Env, algorithm: BaseAlgorithm, apply_grad_cam=False):
         self.evaluation_environment = eval_env
         self.algorithm = algorithm
 
         self.evaluation_timesteps = 100000
         self.episode_length = 1500
 
+        self.apply_grad_cam = apply_grad_cam
 
     def evaluate(self, ):
         state =  self.evaluation_environment.reset()
@@ -33,7 +37,8 @@ class Evaluator:
                 action, _states = self.algorithm.predict(state, deterministic=True)
                 print(f"Action: {action}")
                 state, reward, done, info = self.evaluation_environment.step(action)
-                self.apply_grad_cam(state)
+                if self.apply_grad_cam:
+                    self.show_grad_cam(state)
                 total_reward += reward
                 timesteps += 1
                 current_episode_length += 1
@@ -44,7 +49,7 @@ class Evaluator:
         avg_reward = total_reward / timesteps
         print(f"AVERAGE REWARD: {avg_reward}")
 
-    def apply_grad_cam(self, obs):
+    def show_grad_cam(self, obs):
         policy_net = self.algorithm.policy
         last_cnn_layer = policy_net.features_extractor.extractors.camera_rgb.cnn[0]
 
@@ -102,7 +107,7 @@ class Evaluator:
 
         #plt.subplot(1, 2, 2)
         #plt.title("Grad-CAM Visualization")
-        cv2.imshow("gradients", overlay)
+        cv2.imshow("Grad-CAM Visualization", overlay)
 
         cv2.waitKey(1)
         # Remove hooks after use
@@ -111,3 +116,30 @@ class Evaluator:
 
         activations = {}
         gradients = {}
+
+    def grad_cam2(self, obs):
+        policy_net = self.algorithm.policy
+        last_cnn_layer = policy_net.features_extractor.extractors.camera_rgb.cnn[-2]
+        target_layers = [last_cnn_layer]
+
+        tensor = obs_as_tensor(obs, device=torch.device("cpu"))
+        tensor = preprocess_obs(tensor, self.algorithm.observation_space)
+        tensor["camera_rgb"] = tensor["camera_rgb"].permute(2,0,1).unsqueeze(0)
+        input_tensor = tensor
+        # Note: input_tensor can be a batch tensor with several images!
+
+        # We have to specify the target we want to generate the CAM for.
+        targets = [RawScoresOutputTarget()]
+
+        # Construct the CAM object once, and then re-use it on many images.
+        with GradCAM(model=policy_net, target_layers=target_layers) as cam:
+            # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+            grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
+            # In this example grayscale_cam has only one image in the batch:
+            grayscale_cam = grayscale_cam[0, :]
+            visualization = show_cam_on_image(tensor["camera_rgb"].squeeze(0).permute(1,2,0).numpy(), grayscale_cam, use_rgb=True)
+            # You can also get the model outputs without having to redo inference
+            #model_outputs = cam.outputs
+            cv2.imshow("Grad-CAM Visualization", visualization)
+
+            cv2.waitKey(1)
