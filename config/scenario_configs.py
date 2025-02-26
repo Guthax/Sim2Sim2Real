4f94cbaf11e4a4777410ee29c118c8ac6fcaabf6
Guthax <jurrbuitenweg@gmail.com>
2025-02-26 17:30:48 +0100

    ADded command line tool for easy execution on hpc

--- config/scenario_configs.py
+++ config/scenario_configs.py
@@ -6,6 +6,7 @@ import gymnasium as gym
 from config.environment_configs import environment_configs
 
 _CONFIG_PPO_CNN_DUCKIETOWN_RGB_STEER = {
+    "name": "scenario_1",
     "algorithm": "PPO",
     "algorithm_policy_network": "CnnPolicy",
     "algorithm_hyperparams": algorithm_params["PPO"],
@@ -16,3 +17,24 @@ _CONFIG_PPO_CNN_DUCKIETOWN_RGB_STEER = {
     }
 
 }
+
+_CONFIG_PPO_MULTI_DUCKIETOWN_RGB_CANNY_STEER = {
+    "name": "scenario_2",
+    "algorithm": "PPO",
+    "algorithm_policy_network": "MultiInputPolicy",
+    "algorithm_hyperparams": algorithm_params["PPO"],
+    "observation_space": gym.spaces.Dict({
+            "camera_rgb": gym.spaces.Box(low=0, high=255, shape=(80, 160, 3), dtype=np.uint8),
+            "camera_canny": gym.spaces.Box(low=0, high=255, shape=(80, 160), dtype=np.uint8)
+    }),
+    "action_space": gym.spaces.Box(low=np.array([-1]), high=np.array([1]), dtype=np.float32),
+    "environments": {
+        "duckietown": environment_configs["duckietown_rgb_canny"],
+    }
+
+}
+
+configs = [_CONFIG_PPO_CNN_DUCKIETOWN_RGB_STEER, _CONFIG_PPO_MULTI_DUCKIETOWN_RGB_CANNY_STEER]
+
+def get_config_by_name(name: str):
+    return next((item for item in configs if item['name'] == name), None)
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from util.scenario import  Scenario
-from config.scenario_configs import _CONFIG_PPO_CNN_DUCKIETOWN_RGB_STEER
+from config.scenario_configs import _CONFIG_PPO_CNN_DUCKIETOWN_RGB_STEER, _CONFIG_PPO_MULTI_DUCKIETOWN_RGB_CANNY_STEER
 
-scenario = Scenario(_CONFIG_PPO_CNN_DUCKIETOWN_RGB_STEER)
+import argparse
+
+parser = argparse.ArgumentParser("runner")
+parser.add_argument("config_name", help="The name of the config to use.", type=str)
+parser.add_argument("env_name", help="Environment to train/evaluate on", t)
+args = parser.parse_args()
+print(args.counter + 1)
+
+
+scenario = Scenario(_CONFIG_PPO_MULTI_DUCKIETOWN_RGB_CANNY_STEER)
 scenario.train_on_environment("duckietown",
-                              "duckietown_rgb_cnn",
+                              "duckietown_multi_rgb_canny",
                               "results",
                               "tensorboard",
                               1000000, 5)
