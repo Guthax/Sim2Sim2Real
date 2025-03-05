1c77410c87f1c53dbfaa0801c3a4b051ff3f8dd7
Jurriaan <jurrbuitenweg@gmail.com>
2025-03-05 22:28:22 +0100

    Comment randomization

--- simulators/carla/carla_env.py
+++ simulators/carla/carla_env.py
@@ -129,10 +129,10 @@ class SelfCarlaEnv(gym.Env):
         for actor in self.actor_list:
             actor.destroy()
 
-        if self.count_until_randomization >= self.randomize_every_steps:
-            self._randomize_time_of_day()
-            self._randomize_weather()
-            self.count_until_randomization = 0
+        #if self.count_until_randomization >= self.randomize_every_steps:
+        #    self._randomize_time_of_day()
+        #    self._randomize_weather()
+        #    self.count_until_randomization = 0
 
         self.actor_list = []
 
