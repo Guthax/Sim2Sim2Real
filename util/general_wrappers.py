ddd3412271ebf7a8a7ec914457478705381f89dc
Jurriaan <jurrbuitenweg@gmail.com>
2025-04-12 10:31:33 +0200

    fix

--- util/general_wrappers.py
+++ util/general_wrappers.py
@@ -96,12 +96,13 @@ class OneHotEncodeSegWrapper(gym.ObservationWrapper):
 
     def __init__(self, env=None):
         gym.ObservationWrapper.__init__(self, env)
-        h,w = self.observation_space.shape[0], self.observation_space.shape[1]
 
         if isinstance(self.env.observation_space, spaces.Dict):
+            h, w = self.observation_space["camera_seg"].shape[0], self.observation_space["camera_seg"].shape[1]
             self.observation_space["camera_rgb"] = self.observation_space["camera_rgb"]
             self.observation_space["camera_seg"] = spaces.Box(low=0, high=255, shape=(len(self.color_map), h, w), dtype=np.uint8)
         else:
+            h, w = self.observation_space.shape[0], self.observation_space.shape[1]
             self.observation_space = spaces.Box(low=0, high=255, shape=(len(self.color_map), h, w), dtype=np.uint8)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
