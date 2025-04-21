0fdabcb6edb09a3d531311742bbf889f6b40be11
Guthax <jurrbuitenweg@gmail.com>
2025-04-21 11:36:59 +0200

    fix obs

--- config/scenario_configs.py
+++ config/scenario_configs.py
@@ -11,7 +11,7 @@ _CONFIG_CARLA_RGB = {
     "algorithm_policy_network": "MultiInputPolicy",
     "algorithm_hyperparams": algorithm_params["PPO"],
     "observation_space": gym.spaces.Dict({
-                "camera_rgb": gym.spaces.Box(low=0, high=255, shape=(3, 120, 160), dtype=np.float32),
+                "camera_rgb": gym.spaces.Box(low=0, high=1, shape=(3, 120, 160), dtype=np.float32),
                 "vehicle_dynamics": gym.spaces.Box(np.float32(-1), high=np.float32(1)),
             }),
     "action_space": gym.spaces.Box(np.float32(-1), high=np.float32(1)),
